507320cd857f0c81c997b15a956e047f6bcb072f
Raylene <smith.raylene@gmail.com>
2018-11-24 12:11:32 +0100

    Added create a post test; passes

--- spec/user_controller_spec.rb
+++ spec/user_controller_spec.rb
@@ -8,18 +8,18 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  # describe 'POST /' do
+  describe 'POST /' do
   #   it 'responds with 200' do
   #     post :create, params: { user: { name: 'Jeff', email: 'jeff@hotmail.com', password: 'password1' } }
   #     expect(response).to redirect_to(posts_url)
   #   end
-  #
-  #   it 'creates a post' do
-  #     post :create, params: { user: { name: 'Jeff', email: 'jeff@hotmail.com', password: 'password1' } }
-  #     expect(User.find_by(name: 'Jeff')).to be
-  #   end
-  # end
-  #
+
+    it 'creates a post' do
+      post :create, params: { user: { name: 'Jeff', email: 'jeff@hotmail.com', password: 'password1' } }
+      expect(User.find_by(name: 'Jeff')).to be
+    end
+  end
+
   describe 'GET /' do
     it 'responds with 200' do
       get :index
