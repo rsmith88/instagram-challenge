2eb13047a453703f2844fe87eebe9ec8a45be52d
Raylene <smith.raylene@gmail.com>
2018-11-25 14:59:19 +0100

    Added flash notice for invalid entries in user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,13 @@ class UsersController < ApplicationController
 
   def create
   @user = User.create(user_params)
-      @user.save # Start with "if"
+    if @user.save
       # log_in @user
       redirect_to users_new_url # posts_url
-    # else
-    #   flash.now[:danger] = '<ul> Invalid email or password: <li> Password must be 6-10 characters long </li> <li> Email must include an @ symbol </li></ul>'
-    #   render 'new'
+    else
+      flash.now[:danger] = '<ul> Invalid email or password: <li> Password must be 6-10 characters long </li> <li> Email must include an @ symbol </li></ul>'
+      render 'new'
+    end
   end
 
   def index
