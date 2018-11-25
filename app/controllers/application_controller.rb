d77f45e60ea7dda0a4c95366d8a613dcd100518e
Raylene <smith.raylene@gmail.com>
2018-11-25 15:47:59 +0100

    Updated routers and application controller to redirect index to login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
 
   def redirect_to_login
+    redirect_to sessions_new_url
+  end
+
+  def redirect_to_signup
     redirect_to users_new_url
   end
 
